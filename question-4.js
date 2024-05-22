603b21effb3e37d7db8d7e44de9300d055661195
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-05-22 14:12:50 +0700

    question-4

--- question-4.js
+++ question-4.js
@@ -5,3 +5,14 @@ const inventory = [
   { name: "Orange", price: 30, quantity: 60 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+
+const findLowestStock = ((inven) => {
+  let initialNum = inven[0]["quantity"]; 
+  for (let list of inven){
+    if (list["quantity"] < initialNum){
+      return initialNum = list
+    }
+  }
+})
+  
+console.log(`สินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้าคือ ${findLowestStock(inventory)["name"]} ซึ่งมี ${findLowestStock(inventory)["quantity"]} ชิ้น` )
