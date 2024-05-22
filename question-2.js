e0e2609765e540a5fa7fd800c800418e18fafd7f
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-05-22 14:24:16 +0700

    exercise-2

--- question-2.js
+++ question-2.js
@@ -4,3 +4,23 @@ const inventory = [
   { name: "Banana", price: 10, quantity: 50 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+
+inventory[0]["quantity"] = 200
+inventory.push({ name: "Orange", price: 20, quantity: 300 })
+
+const calculateStockValue = ((array) => {
+  let fruitValue = [];
+  for (let eachFruit of array){
+      fruitValue.push(eachFruit["price"] * eachFruit["quantity"])   
+  }
+  
+  let result = 0;
+  for (let number of fruitValue){
+    result += number
+  }
+
+  return result
+})
+
+
+console.log(calculateStockValue(inventory));
